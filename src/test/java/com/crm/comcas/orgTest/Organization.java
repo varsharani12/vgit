18f9b12af6c0ef632b43dc04c77cc98c0debe0c0
HP <HP@Varsha-R>
2022-02-02 10:07:56 +0530

    This is my first vtiger project

---
+++ src/test/java/com/crm/comcas/orgTest/Organization.java
@@ -0,0 +1,18 @@
+package com.crm.comcas.orgTest;
+
+import org.testng.annotations.Test;
+
+public class Organization {
+	@Test
+
+	public void createOrgTest() {
+		System.out.println("execute CreateOrgTest");
+	}
+	@Test
+
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
+
